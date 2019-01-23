5751d35de98d816a984d5212c3d690ee0c63e13a
alittlecross <alittlecross@live.co.uk>
2019-01-23 15:24:08 +0000

    created banner

---
+++ src/banner.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Banner extends Component {
+
+  render() {
+    return (
+      <div id='banner'>
+        <div id='chin-chin-logo'><img id='logo' src='negroni.ico'></img>Chin Chin</div>
+      </div>
+    )
+  }
+}
+
+export default Banner;
