d22ff5db9c76f455958c52b8dd75a80a4c571b6b
alittlecross <alittlecross@live.co.uk>
2019-01-24 13:41:21 +0000

    corrected App.js name reset

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './main.css';
-import App from './app';
+import App from './App';
 
 import * as serviceWorker from './serviceWorker';
 
