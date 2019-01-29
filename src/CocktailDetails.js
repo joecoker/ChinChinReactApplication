2295af8c507381d0a47d231249f391ec27dc01e8
Paul Fazackerley <alittlecross@live.co.uk>
2019-01-29 11:45:39 +0000

    reverted changes

--- src/CocktailDetails.js
+++ src/CocktailDetails.js
@@ -1,30 +1,36 @@
 import React, { Component } from 'react';
-import CocktailDetailsItem from './CocktailDetailsItem';
-
-export default class CocktailDetails extends Component {
-
-  state = {
-    cocktail: []
-  }
-
-  componentDidMount() {
-    fetch('https://chinchinapi.herokuapp.com/cocktails/name/' + this.props.name)
-      .then(res => res.json())
-      .then(result => {
-          this.setState({cocktail: [result]})
-        }
-      )
-  }
-
-  render() {
-    const { cocktail } = this.state;
-    return (
-      <div>
-        {
-          cocktail.map(cocktail =>
-            <CocktailDetailsItem cocktail={cocktail} />)
-        }
-      </div>
-    )
-  }
-}
+	
+	import CocktailDetailsItem from './CocktailDetailsItem';
+	
+	class CocktailDetails extends Component {
+	
+	  constructor(props) {
+	    super(props);
+	    this.state = {
+	      cocktail: []
+	    };
+	
+	  componentDidMount() {
+	    fetch('https://chinchinapi.herokuapp.com/cocktails/name/' + this.props.name)
+	      .then(res => res.json())
+	      .then(result => {
+	          this.setState({
+	            cocktail: result
+	          });
+	        }
+	      )
+	  }
+      
+    render() {
+	    const { cocktail } = this.state;
+	    return (
+	      <div>
+	      {cocktail.map(cocktail => {
+	        return <CocktailDetailsItem cocktail={cocktail} />
+	      })}
+	      </div>
+	    )
+	  }
+	}
+	
+	export default CocktailDetails;
