e637604d434c982133c78f692a85d7fe1014679e
joecoker <joe_coker@hotmail.co.uk>
2019-01-29 11:14:46 +0000

    Change the icons to negroni

--- src/CocktailDetails.js
+++ src/CocktailDetails.js
@@ -1,23 +1,17 @@
 import React, { Component } from 'react';
-
 import CocktailDetailsItem from './CocktailDetailsItem';
 
-class CocktailDetails extends Component {
+export default class CocktailDetails extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      cocktail: []
-    };
+  state = {
+    cocktail: []
   }
 
   componentDidMount() {
-    fetch('https://chinchinapi.herokuapp.com/cocktails/name/'+this.props.name)
+    fetch('https://chinchinapi.herokuapp.com/cocktails/name/'+ this.props.name)
       .then(res => res.json())
       .then(result => {
-          this.setState({
-            cocktail: result
-          });
+          this.setState({cocktail: [result]})
         }
       )
   }
@@ -26,12 +20,11 @@ class CocktailDetails extends Component {
     const { cocktail } = this.state;
     return (
       <div>
-      {cocktail.map(cocktail => {
-        return <CocktailDetailsItem cocktail={cocktail} />
-      })}
+        {
+          cocktail.map(cocktail =>
+            <CocktailDetailsItem cocktail={cocktail} />)
+        }
       </div>
     )
   }
 }
-
-export default CocktailDetails;
